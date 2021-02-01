571bd0264659659e2e0b0ab0d6e04cd920f987b0
betoenlaweb <72509029+betoenlaweb@users.noreply.github.com>
2021-02-01 16:31:16 -0500

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -1,72 +1,139 @@
+////////////////////////////////////////////////////   PAGE LOAD    //////////////////////////////////////////////////////////
 
-//////// JQUERY NAV-BAR///////////////
+window.onload = function() {
+  const loadPage = document.querySelector(".load");
 
-const nav = window.matchMedia('(-webkit-min-device-pixel-ratio:0) and (min-width: 1024px)')
+  this.setTimeout(() => {
+    loadPage.classList.remove('active')
+  }, 300);
 
-function fullScreen(n) {
-  
-  if(n.matches) {
-      $('#openNav').on('click', function(nav) {    
-      document.getElementById("myNav").style.height = "100%";
-    });
-  }else {
-    $('#openNav').on('click', function(nav) {    
-      document.getElementById("myNav").style.height = "15%";
-    });
+  for(let i = 0; i < itemMenu.length; i++) {
+    const items = itemMenu[i];
+
+
+    items.onclick = e => {
+      e.preventDefault();
+      let target = e.target.href;
+      for(let i =0; i < itemMenu.length; i++) {
+        itemMenu[i].style.cssText = "opacity: 0; transition: 0.3s; transition-delay: 0.1s; line-height: 1.2";
+      }
+      
+      setTimeout(() => {
+        window.location.href = target;
+      }, 300);
+    }
   }
 }
 
-nav.addListener(fullScreen)
-fullScreen(nav)
+//////////////////////////////  ////////  NAV BAR  ///////
 
-$('#closeNav').on('click', function() {
-  document.getElementById("myNav").style.height = "0%";
+$('#close-menu').on('click', function() {
+  document.querySelector(".menu").style.transform = "scaleY(0)";
+
+  for(let i = 0; i < itemMenu.length; i++) {
+    itemMenu[i].style.cssText = "opacity: 0; transition: 0.3s; ";
+  }
 });
 
-$('.overlay-content').on('click', function() {
-  document.getElementById("myNav").style.height = "0%";
+$('#open-menu').on('click', function() {    
+  document.querySelector(".menu").style.transform = "scaleY(1)";
+  for(let i = 0; i < itemMenu.length; i++) {
+    itemMenu[i].style.cssText = "opacity: 1; transition: 0.3s; transition-delay: 0.27s; line-height: 1.2;";
+  }
+});
+let itemMenu = document.querySelectorAll(".item-menu")
+
+//////////ROTATION-BOTTON-MENU//////////////
+
+$('#open-menu').on('click', function() {
+  document.querySelector(".bottom-menu").style.transform = "rotate(45deg)";
 });
 
-//////////LOGO-GIF//////////////////////
+$('#close-menu').on('click', function() {
+  document.querySelector(".bottom-menu").style.transform = "rotate(0deg)";
+});
+
+$('#open-menu').on('click', function() {
+  document.querySelector(".close-btn").style.cssText = "transform: rotate(0deg); opacity: 1";
+});
+
+$('#close-menu').on('click', function() {
+  document.querySelector(".close-btn").style.cssText = "transform: rotate(-45deg); opacity: 0; transition: 0.2s";
+});
 
 
+///////////////////////MODAL///////////////////////////
 
 
+let hover = document.querySelectorAll(".box-item")
+let fadeIn = document.querySelectorAll(".item-hover");
+let fadeOut = document.querySelectorAll(".close-modall");
 
+let modal = document.querySelectorAll(".modall");
+let modalContent = document.querySelectorAll(".modall-content");
+let video = document.querySelectorAll("#video");
+let fade = document.querySelectorAll("#fade-in");
+let tittle = document.querySelectorAll(".tittle");
+let description = document.querySelectorAll(".description");
 
+for(let i = 0; i < fadeIn.length; i++) {
+  fadeIn[i].onclick = function() {
 
+    for(let i =0; i < video.length; i++) {
+      video[i].style.cssText = "opacity: 1; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: .8s"
+    }
 
-///////////JAVA SCRIPT MODAL/////////////
+    for(let i =0; i < fade.length; i++) {
+      fade[i].style.cssText = "opacity: 1; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: 1s";
+    }
 
+    for(let i =0; i < tittle.length; i++) {
+      tittle[i].style.cssText = "opacity: 1; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: 1s";
+    }
 
-let blurIn = document.querySelectorAll(".modal-open");
-let blur = document.querySelectorAll(".modal-area"); 
-let blurOut = document.querySelectorAll(".modal-close");
+    for(let i =0; i< description.length; i++) {
+      description[i].style.cssText = "opacity: 1; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: 1.2s";
+    }
 
+  };
+}
 
+for(let i = 0; i < fadeOut.length; i++) {
+  fadeOut[i].onclick = function() {
 
-for(let i = 0; i < blurIn.length; i++) {
   
-  blurIn[i].onclick = function() {
-    for(let i =0; i< blur.length; i++) {
-      blur[i].style.cssText = "opacity: 1; transition: 0.4s; transition-delay: 0.8s";
+    for(let i =0; i < video.length; i++) {
+      video[i].style.cssText = "opacity: 0; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: .8s";
+    }
+
+    for(let i =0; i < fade.length; i++) {
+      fade[i].style.cssText = "opacity: 0; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: 1s";
+    }
+
+    for(let i =0; i < tittle.length; i++) {
+      tittle[i].style.cssText = "opacity: 0; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: 1s";
+    }
+
+    for(let i =0; i < description.length; i++) {
+      description[i].style.cssText = "opacity: 0; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55); transition-delay: 1.2s";
     }
   };
- 
 }
 
 
-for(let i = 0; i < blurOut.length; i++) {
+////////////////////////////////////////////////////   ITEM HOVER   //////////////////////////////////////////////////////////
+
+for(let i = 0; i < hover.length; i++) {
+  hover[i].ontouchstart  = function() {
+    fadeIn[i].style.cssText = "opacity: 1; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55)";
   
-  blurOut[i].onclick = function() {
-    for(let i =0; i< blur.length; i++) {
-      blur[i].style.cssText = "opacity: 0; transition: 0.4s; transition-delay: 0.8s";
-    }
   };
- 
 }
-   
 
-
-
+for(let i = 0; i < hover.length; i++) {
+  hover[i].ontouchend = function() {
+    fadeIn[i].style.cssText = "opacity: 0; transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55)";
+  
+  };
+}
 
